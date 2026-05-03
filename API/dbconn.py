75543df3c5b39e45b7c5118b3099ff4b0a7431c2
Jack Bauer <jgb1017@gmail.com>
2026-05-03 10:58:08 -0500

    Setup API to DB connection

--- API/dbconn.py
+++ API/dbconn.py
@@ -9,8 +9,14 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-conn = None
-try:
+def db_get_version(conn):
+    cur = conn.cursor()
+    cur.execute('SELECT VERSION();')
+    result = (cur.fetchone()[0])
+    cur.close()
+    return result
+
+def db_connect():
     conn = mysql.connector.connect(
         host=DB_HOST,
         port=DB_PORT,
@@ -18,16 +24,20 @@ try:
         user=DB_USER,
         password=DB_PASSWORD,
         ssl_disabled=False,
-        autocommit=True,
-    ssl_ca='./global-bundle.pem'
-    )
-    cur = conn.cursor()
-    cur.execute('SELECT VERSION();')
-    print(cur.fetchone()[0])
-    cur.close()
-except Exception as e:
-    print(f"Database error: {e}")
-    raise
-finally:
-    if conn:
-        conn.close()
+        autocommit=True)
+    return conn
+
+# queryFunc must be a function that takes 
+def db_run_query(queryFunc):
+    try:
+        conn = db_connect()
+        return queryFunc(conn)
+    except Exception as e:
+        print(f"Database error: {e}")
+        raise
+    finally:
+        if conn:
+            conn.close()
+
+def db_test_conn():
+    return db_run_query(db_get_version)
--- API/server.py
+++ API/server.py
@@ -1,16 +1,25 @@
+# System imports
 from flask import Flask
 from dotenv import load_dotenv
 import os
 
+# Server imports
+from dbconn import db_test_conn
+
+# Load environment variables
 load_dotenv() # Get .env file variables
 LOCAL = os.getenv("LOCAL")
 
+# Create a flask app
 app = Flask(__name__)
 
+# Flask app routes
 @app.route("/")
 def hello_world():
-    return {"message": "Hello, world!"}
+    version = db_test_conn()
+    return {"message": version}
 
+# If we're in a local build, run the server this way
 if LOCAL:
     # Vercel handles running the app non-locally for us
     if __name__ == "__main__":
