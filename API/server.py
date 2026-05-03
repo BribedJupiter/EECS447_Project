04eb92c335715a51799b44d35282736deffe7c2d
Jack Bauer <jgb1017@gmail.com>
2026-05-03 10:40:57 -0500

    Local / cloud distinction in server

--- API/server.py
+++ API/server.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # Get .env file variables
+LOCAL = os.getenv("LOCAL")
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return {"message": "Hello, world!"}
+
+if LOCAL:
+    # Vercel handles running the app non-locally for us
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=8000, debug=True)
